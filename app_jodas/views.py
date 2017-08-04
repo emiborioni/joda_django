b8383cf5e2977babffb34a14b3b6ca6b48277f3f
Alcachofer <spadonienzo@gmail.com>
2017-08-04 11:32:23 -0300

    primer commit

---
+++ app_jodas/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required 
+from django.core.urlresolvers import reverse 
+from django.shortcuts import render_to_response, render, redirect 
+from django.template import RequestContext 
+from django.conf import settings 
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse 
+from django.contrib.auth import login, logout, authenticate 
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib import messages 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
+from django.db.models import Q 
+from datetime import datetime, timedelta 
+from django.utils import timezone
+from .models import *
+# Create your views here.
